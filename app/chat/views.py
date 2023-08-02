e941d32d7d1de7bd04a1fa59c340d4f8b92478f7
th3maid <36008397+th3maid@users.noreply.github.com>
2023-08-02 06:22:34 -0300

    add web ui frontend

--- app/chat/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    context = {"value": "a"}
-    return render(request, "chat/index.html", context)
---
+++ assets/main.js
@@ -0,0 +1,8 @@
+function tg_btn() {
+    var x = document.getElementById("sidebar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
