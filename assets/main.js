39cf867e36966bfef70bf37050b64546171ca703
th3maid <36008397+th3maid@users.noreply.github.com>
2023-08-02 22:45:48 -0300

    remove old files

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-function tg_btn() {
-    var x = document.getElementById("sidebar");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
